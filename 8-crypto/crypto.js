ab29be73dfc6aa0bc1efd7836a148792003e948e
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-02-06 23:44:41 +0800

    Выделил в отдельную функцию

--- 8-crypto/crypto.js
+++ 8-crypto/crypto.js
@@ -5,43 +5,41 @@ const password = 'СУПЕР_ПАРОЛЬ';
 const key = 'РЕПУСЬЛОРАП_';
 const invalidKey = 'НЕПОДХОДЯЩИЙ_КЛЮЧ';
 
-// Шифратор пароля
-function coder(password) {
-  if (password.length < 5) {
+// Шифрока и дешифровка
+function crypto(input) {
+  if (input.length < 5) {
     console.log('Пароль должен быть длиннее 5 символов');
     return null;
   }
 
-  const splitPassword = password.split('');
+  const splitInput = input.split('');
 
-  let [i0, i1, i2, i3, i4, ...other] = splitPassword;
+  let [i0, i1, i2, i3, i4, ...other] = splitInput;
   [i0, i4] = [i4, i0];
   [i1, i3] = [i3, i1];
-  const newOtherPassword = other.reverse();
+  const newOther = other.reverse();
 
-  const crypted = [i0, i1, i2, i3, i4].concat(newOtherPassword).join('');
+  return [i0, i1, i2, i3, i4].concat(newOther).join('');
+}
 
-  console.log('Зашифрованный пароль:', crypted);
-  return crypted; // Зашифрованный пароль
+// Зашифровка пароля
+function coder(password) {
+  const crypted = crypto(password);
+  if (crypted) console.log('Зашифрованный пароль:', crypted);
+  return crypted;
 }
 
 // Проверка
 function check(password, key) {
-  const splitKey = key.split('');
-  let [i0, i1, i2, i3, i4, ...other] = splitKey;
-  [i4, i0] = [i0, i4];
-  [i3, i1] = [i1, i3];
-  const newOtherKey = other.reverse();
-  const deCrypted = [i0, i1, i2, i3, i4].concat(newOtherKey).join('');
-
+  const deCrypted = crypto(key);
   console.log('Раcшифрованный пароль:', deCrypted);
 
-  // Проверяем
   password === deCrypted
-    ? console.log('Проверка пройдена', password === deCrypted)
-    : console.log('Задание провалено', password === deCrypted);
+    ? console.log(password === deCrypted, 'Проверка пройдена')
+    : console.log(password === deCrypted, 'Задание провалено');
 }
 
+// Тестирование
 coder(password);
 check(password, key);
 console.log('___________________________________________');
